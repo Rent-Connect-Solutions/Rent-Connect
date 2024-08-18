caec546fb38a1ad1549321f49ddb3357897926d3
Aaron Katz <aaron.katz.mobile@gmail.com>
2024-08-18 16:20:48 -0400

    Updated call to action popup and mailchimp signups

---
+++ js/modalControls.js
@@ -0,0 +1,69 @@
+    // Get the login modal popup
+    var modal = document.getElementById('id01');
+    
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+
+    // Get the registration modal popup
+    var modal = document.getElementById('id02');
+    
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+
+    // Get the tenantpost modal popup
+    var modal = document.getElementById('id03');
+    
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }        
+
+    // Get the propertymanagerpost modal popup
+    var modal = document.getElementById('id04');
+    
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }        
+
+    // Get the landlord mailchimp modal popup
+    var modal = document.getElementById('id05');
+    
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }        
+
+    // Get the tenant mailchimp modal popup
+    var modal = document.getElementById('id06');
+    
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }    
+
+    // Get the propertymanager mailchimp modal popup
+    var modal = document.getElementById('id07');
+    
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }    
